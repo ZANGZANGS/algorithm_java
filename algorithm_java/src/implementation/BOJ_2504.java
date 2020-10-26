9f326671e02be5a7f900a17d2be85b269ddccdee
zangzangs <kimjangh94@gmail.com>
2020-10-26 23:48:53 +0900

    no message

--- algorithm_java/src/implementation/BOJ_2504.java
+++ algorithm_java/src/implementation/BOJ_2504.java
@@ -3,6 +3,7 @@ package implementation;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Stack;
 
 public class BOJ_2504 {
 
@@ -11,29 +12,82 @@ public class BOJ_2504 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		String input = br.readLine();
-		String temp = String.valueOf(input);
 		
-		int strLength = 0;
-		while (temp.length() != strLength) {
-			strLength = temp.length();
-
-			temp = temp.replace("()", "");	
-			temp = temp.replace("[]", "");	
+		Stack<String> stack = new Stack<>();
+		int num = 0;
+		boolean isGood = true;
+		
+		for (int i = 0; i < input.length(); i++) {
+			
+			
+			String at = input.substring(i, i+1);
+			if("(".equals(at)) {
+				stack.push(")");
+				continue;
+			}
+			
+			if("[".equals(at)) {
+				stack.push("]");
+				continue;
+			}
+			
+			while(true) {
+				if(stack.isEmpty()) {
+					isGood = false;
+					break;
+				}
+				
+				if(isNumber(stack.peek())) {
+					num += Integer.parseInt(stack.pop());
+				}else {
+					if(at.equals(stack.peek())) {
+						stack.pop();
+						if(num != 0) {
+							if(")".equals(at)) {
+								stack.push(Integer.toString(2*num));
+								num = 0;
+							}else {	//"]"
+								stack.push(Integer.toString(3*num));
+								num = 0;
+							}
+						}else {
+							if(")".equals(at)) {
+								stack.push(Integer.toString(2));
+							}else {	//"]"
+								stack.push(Integer.toString(3));
+							}
+						}
+						break;
+					}else {
+						isGood= false;
+						break;
+					}
+				}
+			}
+			if(!isGood) break;
+			
+			
 		}
 		
-		if("".equals(temp)) {
-			System.out.println(0);
-		}else {
-			input = input.replace("()", "2");
-			input = input.replace("[]", "3");
+		int result=0;
+		while (!stack.empty()) {
+			if(isNumber(stack.peek())) {
+				result += Integer.parseInt(stack.pop());
+			}else {
+				isGood = false;
+				break;
+			}
 		}
-		
-		//(()[[]])([]))
-		
-		//(2[3])(3))
-		
-		//(()[[]])([]))
+		result = isGood ? result : 0 ;
+		System.out.println(result);
 		
 	}
 
+	public static boolean isNumber(String input) {
+		if(")".equals(input) || "]".equals(input)) {
+			return false;
+		}else {
+			return true;
+		}
+	}
 }
