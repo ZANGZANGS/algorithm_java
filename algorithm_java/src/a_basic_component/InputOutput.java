afc7f635ebddb90c324a370941107d9e407dc311
zangzangs <kimjangh94@gmail.com>
2021-03-15 23:37:05 +0900

    in out  templete

---
+++ algorithm_java/src/a_basic_component/InputOutput.java
@@ -0,0 +1,19 @@
+package a_basic_component;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class InputOutput {
+
+	public static void main(String[] args) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		StringBuilder sb = new StringBuilder();
+		StringBuffer sbu = new StringBuffer();
+		
+	}
+
+}
