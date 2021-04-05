a1c115b37852c92e9cb64fb74c6d9ad68076847f
KimJangHyeon <kimjangh94@gmail.com>
2021-04-05 22:52:08 +0900

    2xn 타일 2

--- algorithm_java/src/dynamicprogramming/BOJ_11727.java
+++ algorithm_java/src/dynamicprogramming/BOJ_11727.java
@@ -11,18 +11,21 @@ public class BOJ_11727  {
 
 		int n = Integer.parseInt(br.readLine());
 		
-		
-		int[] dp = new int[n];
-		
-		dp[0] = 1;
-		dp[1] = 3;
-		
-		for (int i = 2; i < n; i++) {
-			dp[i] = dp[i-1] + dp[i-2]*2; 
-			dp[i] %= 10007;
+		if(n == 1) System.out.println(1);
+		else {
+			int[] dp = new int[n];
+			
+			dp[0] = 1;
+			dp[1] = 3;
+			
+			for (int i = 2; i < n; i++) {
+				dp[i] = dp[i-1] + dp[i-2]*2; 
+				dp[i] %= 10007;
+			}
+			
+			System.out.println(dp[n-1]);
 		}
 		
-		System.out.println(dp[n-1]);
 		
 	}
 
