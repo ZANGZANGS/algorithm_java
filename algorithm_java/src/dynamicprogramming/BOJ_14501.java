84fd025e4ea4c7b85e104ba141fac9d2a59a5f89
KimJangHyeon <kimjangh94@gmail.com>
2021-04-19 23:43:05 +0900

    퇴사

---
+++ algorithm_java/src/dynamicprogramming/BOJ_14501.java
@@ -0,0 +1,40 @@
+package dynamicprogramming;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BOJ_14501  {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int n = Integer.parseInt(br.readLine());
+		
+		int[] jobT = new int[n+10];
+		int[] jobP = new int[n+10];
+		int[] dp = new int[n+10]; // n까지 얻는 최대 이익
+		
+		for (int i = 1; i <= n; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			
+			jobT[i] = Integer.parseInt(st.nextToken()); 
+			jobP[i] = Integer.parseInt(st.nextToken()); 
+		}
+
+		int result = 0;
+
+		for (int i = 1; i <= n+1; i++) {
+			
+			dp[i] = Math.max(dp[i], result);
+            dp[jobT[i]+i] = Math.max(dp[jobT[i]+i],jobP[i]+dp[i]);
+            result = Math.max(result, dp[i]);
+		}
+		
+		System.out.println(result);
+		
+	}
+
+}
