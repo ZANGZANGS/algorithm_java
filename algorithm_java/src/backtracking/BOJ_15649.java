384ff7eb9e111a0548bd21999f79ea6b53ef4657
KimJangHyeon <kimjangh94@gmail.com>
2021-04-21 22:41:51 +0900

    N과 M (1) 재풀이

--- algorithm_java/src/backtracking/BOJ_15649.java
+++ algorithm_java/src/backtracking/BOJ_15649.java
@@ -3,46 +3,105 @@ package backtracking;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class BOJ_15649 {
-	static int n,m;
+	
+	static int N;
+	static int M;
+	static boolean[] isUsed; 
 	static int[] arr;
-	static boolean[] isused;
 	static StringBuilder sb;
 	
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String input = br.readLine();
 		StringTokenizer st = new StringTokenizer(input, " ");
-		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
 		
-		arr = new int[m];
-		isused = new boolean[n+1]; // +1 을 하는 이유는 배열 인덱스가 0 부터 시작해서
-		sb= new StringBuilder();
-		
-		func(0);
-		System.out.print(sb.toString());
-	}
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		arr = new int[M];
+		isUsed = new boolean[N+1];
 
-	static void func(int k) {
+		sb = new StringBuilder();
+		func(0);
 		
-		if(k == m) { // m개를 모두 택했다면
-			for(int i = 0 ; i< m ; i++) {
-				sb.append(arr[i]+" "); // arr에 기록해둔 수를 출력
-			}
+		System.out.println(sb.toString());
+	}
+	
+	static void func(int step) {
+		
+		if(step == M) {
+			Arrays.stream(arr).forEach(v-> sb.append(v).append(" "));
 			sb.append("\n");
 			return;
 		}
 		
-		for(int i = 1; i <= n; i++) { // 숫자 그대로 사용할것임
-			if(!isused[i]) {// 아직 i 가 사용되지 않았으면
-				arr[k] = i; // k번째 수를 i로 정함
-				isused[i] = true; // i를 사용했다고 표시
-				func(k+1); // 다음 수를 정하러 한 단계 더 들어감
-				isused[i] = false;	// k번째 수를 i로 한 모든 경우를 확인했으니 사용하지 않음으로 변경
+		
+		for (int i = 1; i <= N; i++) {
+			
+			if(!isUsed[i]) {
+				isUsed[i] = true;
+				arr[step] = i;
+				func(step+1);
+				isUsed[i] = false;
 			}
+			
 		}
 	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+//	static int n,m;
+//	static int[] arr;
+//	static boolean[] isused;
+//	static StringBuilder sb;
+//	
+//	public static void main(String[] args) throws IOException{
+//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		String input = br.readLine();
+//		StringTokenizer st = new StringTokenizer(input, " ");
+//		n = Integer.parseInt(st.nextToken());
+//		m = Integer.parseInt(st.nextToken());
+//		
+//		arr = new int[m];
+//		isused = new boolean[n+1]; // +1 을 하는 이유는 배열 인덱스가 0 부터 시작해서
+//		sb= new StringBuilder();
+//		
+//		func(0);
+//		System.out.print(sb.toString());
+//	}
+//
+//	static void func(int k) {
+//		
+//		if(k == m) { // m개를 모두 택했다면
+//			for(int i = 0 ; i< m ; i++) {
+//				sb.append(arr[i]+" "); // arr에 기록해둔 수를 출력
+//			}
+//			sb.append("\n");
+//			return;
+//		}
+//		
+//		for(int i = 1; i <= n; i++) { // 숫자 그대로 사용할것임
+//			if(!isused[i]) {// 아직 i 가 사용되지 않았으면
+//				arr[k] = i; // k번째 수를 i로 정함
+//				isused[i] = true; // i를 사용했다고 표시
+//				func(k+1); // 다음 수를 정하러 한 단계 더 들어감
+//				isused[i] = false;	// k번째 수를 i로 한 모든 경우를 확인했으니 사용하지 않음으로 변경
+//			}
+//		}
+//	}
 }
