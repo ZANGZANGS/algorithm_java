f817dad663f8592703760b39bcc7938136653d17
DESKTOP-912KDV6\kimja <>
2021-08-06 00:08:25 +0900

    1278 미로 재풀이

--- algorithm_java/src/breadthFirstSearch/BOJ_2178.java
+++ algorithm_java/src/breadthFirstSearch/BOJ_2178.java
@@ -2,70 +2,69 @@ package breadthFirstSearch;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.StringTokenizer;
 
+/**
+ * @source		: 백준
+ * @algorithm	: BFS
+ * @description	: 미로탐색
+ * ==============================================
+ * DATE			NOTE	
+ * ==============================================
+ * 2021.08.05	조건에 맞는 좌표는 Queue에 넣자.
+ */
 public class BOJ_2178 {
 
 	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		String nm = br.readLine();
-		
-		int n = Integer.parseInt(nm.split(" ")[0]);
-		int m = Integer.parseInt(nm.split(" ")[1]);
-		
-		int[][] miro = new int[n][m];
-		int[][] dist = new int[n][m];
-		
-		Queue<Dot> que = new LinkedList<Dot>();
+		BufferedReader br = new BufferedReader(new java.io.InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 		
 		int[] dx = {0,1,0,-1};
 		int[] dy = {1,0,-1,0};
 		
+		int N = Integer.parseInt(st.nextToken());	//Y
+		int M = Integer.parseInt(st.nextToken());	//X
 		
-		for (int i = 0; i < n; i++) {
-			String input = br.readLine();
-			for (int j = 0; j < m; j++) {
-				miro[i][j] = input.charAt(j) -'0';
+		int[][] arr = new int[N][M];
+		boolean[][] vis= new boolean[N][M];
+		
+		Queue<int[]> Q = new LinkedList<int[]>();
+		
+		for (int i = 0; i < N; i++) {
+			char[] in = br.readLine().toCharArray();
+			for (int j = 0; j < M; j++) {
+				arr[i][j] = in[j] -'0';
 			}
 		}
 		
-		//bfs
+		Q.add(new int[] {0,0});	//order by x,y 
+		vis[0][0] = true;
 		
-		dist[0][0] = 1;
-		que.add(new Dot(0,0));
-		
-		while (!que.isEmpty()) {
-			Dot dot = que.poll();
-			for (int i = 0; i < 4; i++) {
-				int nx = dot.x+dx[i];
-				int ny = dot.y+dy[i];
-				 
-				if(nx < 0 || ny < 0 || nx >=n || ny >= m) continue;
-				if(miro[nx][ny] == 0 || dist[nx][ny] >0) continue;
+		while(!Q.isEmpty()) {
+			int[] temp = Q.poll();
+			
+			int x = temp[0];
+			int y = temp[1];
+
+			for (int k = 0; k < 4; k++) {
+				int nx = x + dx[k];
+				int ny = y + dy[k];
 				
-				que.add(new Dot(nx, ny));
-				dist[nx][ny]= dist[dot.x][dot.y]+1;
+				if(nx<0 || ny < 0 || nx>= M || ny >= N) continue;
+				if(vis[ny][nx] == true) continue;
+				if(arr[ny][nx] == 0) continue;
 				
+				arr[ny][nx] = arr[y][x] +1;
+				vis[ny][nx] = true;
+				
+				Q.add(new int[] {nx,ny});
 			}
 			
 		}
 		
-		System.out.println(dist[n-1][m-1]);
-	
-	
+		System.out.println(arr[N-1][M-1]);
 		
-	}
-}
-
-class Dot{
-	int x;
-	int y;
-	
-	public Dot(int x, int y) {
-		this.x = x;
-		this.y = y;
 	}
 }
