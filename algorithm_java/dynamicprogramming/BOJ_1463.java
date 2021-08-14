c23dbac9ee54b521869f9c4a3369fb1155ca0f99
김장현 <>
2021-08-14 18:52:56 +0900

    1로 만들기 재풀이

--- algorithm_java/dynamicprogramming/BOJ_1463.java
+++ algorithm_java/dynamicprogramming/BOJ_1463.java
@@ -3,7 +3,15 @@ package dynamicprogramming;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+/**
+ * @source		: 백준
+ * @algorithm	: DP
+ * @description	: 1로 만들기
+ * ==============================================
+ * DATE			NOTE	
+ * ==============================================
+ * 2021.08.14	
+ */
 
 public class BOJ_1463 {
 	
@@ -12,22 +20,15 @@ public class BOJ_1463 {
 		
 		int n = Integer.parseInt(br.readLine());
 		
-		int[] d = new int[n+1];
-		Arrays.fill(d, Integer.MAX_VALUE);
+		int[] dp = new int[n+1];
 		
-		d[1] = 0;
-		for (int i = 2; i <= n; i++) {
-			//1. X가 3으로 나누어 떨어지면 3으로 나눈다.
-			if(i%3 == 0) d[i] = Math.min(d[i], d[i/3]+1);
-			//2. X가 2로 나누어 떨어지면 2로 나눈다.
-			if(i%2 == 0) d[i] = Math.min(d[i], d[i/2]+1);
-			//3. 1을 뺸다.
-			d[i] = Math.min(d[i], d[i-1]+1);
-		}
-		
-		System.out.println(d[n]);
-		
-	}
-	
+		dp[1] = 0;
 
+		for (int i = 2; i < dp.length; i++) {
+			dp[i] = dp[i-1] +1;
+			if(i%3 == 0) dp[i] = Math.min(dp[i], dp[i/3]+1);
+			if(i%2 == 0) dp[i] = Math.min(dp[i], dp[i/2]+1);
+		}
+		System.out.println(dp[n]);
+	}
 }
