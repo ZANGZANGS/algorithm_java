861e6519ec8e69ce47528cd9b13f2ef3d37bf41b
김장현 <>
2021-08-18 23:32:25 +0900

    RGB 사거리 재풀이

--- algorithm_java/dynamicprogramming/BOJ_1149.java
+++ algorithm_java/dynamicprogramming/BOJ_1149.java
@@ -4,47 +4,51 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-
+/**
+ * @source		: 백준
+ * @algorithm	: DP
+ * @description	: RGB거리
+ * ==============================================
+ * DATE			NOTE	
+ * ==============================================
+ * 2021.08.18	
+ */
 public class BOJ_1149 {
 
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		int n = Integer.parseInt(br.readLine());
-		int[][] arr = new int[n+1][3];
+		int N = Integer.parseInt(br.readLine());
+		int[][] arr = new int[N+1][3];
+		int[][] dp = new int[N+1][3];
 		
-		//arr[i][j]
-		// i번째 집에 j(R,G,B) 비용
-		
-		StringTokenizer st ;
-		for (int i = 1; i <= n; i++) {
-			st = new StringTokenizer(br.readLine());
+		for (int i = 1; i <= N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
 			
-			arr[i][0] = Integer.parseInt(st.nextToken());	//R 빨강
-			arr[i][1] = Integer.parseInt(st.nextToken());	//G 초록
-			arr[i][2] = Integer.parseInt(st.nextToken());	//B 파랑
+			for (int j = 0; j < 3; j++) {
+				arr[i][j] = Integer.parseInt(st.nextToken());
+			}
+			
 		}
 		
-		int[][] d = new int[n+1][3]; //거리 정보를 담을 배열
+		//init
+		dp[1][0] = arr[1][0];
+		dp[1][1] = arr[1][1];
+		dp[1][2] = arr[1][2];
 		
-		/*
-		 * i번째 집까지 칠할 떄 비용의 최솟값, j는 색상
-		 d[i][0] R
-		 d[i][1] G
-		 d[i][2] B
-		 */
-		
-		d[1][0] = arr[1][0];
-		d[1][1] = arr[1][1];
-		d[1][2] = arr[1][2];
-		
-		for (int i = 2; i < d.length; i++) {
-			d[i][0] = Math.min(d[i-1][1], d[i-1][2])+arr[i][0];
-			d[i][1] = Math.min(d[i-1][0], d[i-1][2])+arr[i][1];
-			d[i][2] = Math.min(d[i-1][0], d[i-1][1])+arr[i][2];
+		//dp
+		for (int i = 2; i <= N; i++) {
+			dp [i][0] = arr[i][0] + Math.min(dp[i-1][1], dp[i-1][2]);
+			dp [i][1] = arr[i][1] + Math.min(dp[i-1][0], dp[i-1][2]);
+			dp [i][2] = arr[i][2] + Math.min(dp[i-1][0], dp[i-1][1]);
 		}
 		
-		System.out.println(Math.min(d[n][0], Math.min(d[n][1], d[n][2])));
+		//find minimum value
+		int result = Integer.MAX_VALUE;
+		for (int amt : new int[]{dp[N][0], dp[N][1], dp[N][2]}) {
+			result = Math.min(amt, result);
+		}
+		
+		System.out.println(result);
 	}
-
 }
