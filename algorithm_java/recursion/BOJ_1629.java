45959ed08a8ef65358ff034510d4bf4a8761d264
ZANGZANGS 49123158+ZANGZANGS@users.noreply.github.com <kimjangh94@gmail.com>
2021-08-24 23:58:17 +0900

    곰샘 재풀이

--- algorithm_java/recursion/BOJ_1629.java
+++ algorithm_java/recursion/BOJ_1629.java
@@ -1,6 +1,3 @@
-/**
- * a^b %c
- */
 package recursion;
 
 import java.io.BufferedReader;
@@ -8,6 +5,15 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+/**
+ * @source		: baekjoon
+ * @algorithm	: recursion
+ * @description	: 곱샘 a^b %c
+ * ==============================================
+ * DATE			NOTE	
+ * ==============================================
+ * 2021.08.24	TODO 다시 풀기
+ */
 public class BOJ_1629 {
 
 	public static void main(String[] args) throws IOException {
@@ -15,33 +21,33 @@ public class BOJ_1629 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		String input = br.readLine();
-		StringTokenizer st = new StringTokenizer(input, " ");
+		StringTokenizer st = new StringTokenizer(input);
 		
 		long a = Integer.parseInt(st.nextToken());
 		long b = Integer.parseInt(st.nextToken());
 		long c = Integer.parseInt(st.nextToken());
 		
-		//System.out.println((long)(Math.pow(a, b)%c));
 		
-		long result = pow(a,b,c);
-		
-		System.out.println(result);
+		System.out.println(pow(a,b,c));
+	
 		
 	}
 	
+	
 	public static long pow(long a, long b, long c) {
-		if(b==1) {
+		if(b == 1) {
 			return a%c;
-		}else {
-			long val = pow(a,b/2,c);
-			val = (val*val) %c;
-			
-			if(b%2 == 0) {
-				return val;
-			}else {
-				return (val*a) %c;
-			}
 		}
+		
+		long val = pow(a,b/2,c);
+		val = val * val % c;
+		
+		if(b%2 == 0) {
+			return val;
+		}else {
+			return val * a % c;
+		}
+		
 	}
 }
 
