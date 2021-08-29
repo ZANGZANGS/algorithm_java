6b70b2ff281a4dd522b75efc2221011a59fe5193
ZANGZANGS 49123158+ZANGZANGS@users.noreply.github.com <kimjangh94@gmail.com>
2021-08-29 13:12:09 +0900

    숫자 카드 2 - 이분탐색 풀이

--- algorithm_java/binarysearch/BOJ_10816.java
+++ algorithm_java/binarysearch/BOJ_10816.java
@@ -3,11 +3,20 @@ package binarysearch;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 
+/**
+ * @source		: baekjoon
+ * @algorithm	: hashmap, binarysearch
+ * @description	: 숫자 카드 2
+ * ==============================================
+ * DATE			NOTE	
+ * ==============================================
+ * 2021.08.28	hashmap
+ * 2021.08.29	binarysearch
+ */
 public class BOJ_10816 {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
@@ -17,38 +26,62 @@ public class BOJ_10816 {
 		StringTokenizer stringTokenizer;
 		
 		int N = Integer.parseInt(br.readLine());
-		
-		Map<Integer, Integer> map = new HashMap<>();
+		int[] arrN = new int[N];
 		
 		stringTokenizer = new StringTokenizer(br.readLine());	
 		for (int i = 0; i < N; i++) {
-			
-			int number = Integer.parseInt(stringTokenizer.nextToken());
-			
-			Integer tar = map.get(number);
-			
-			if(null == tar) {	//처음이면
-				map.put(number, 1);
-			}else {
-				map.put(number, tar+1);
-			}
+			arrN[i] = Integer.parseInt(stringTokenizer.nextToken()); 
 			
 		}
 		
+		Arrays.sort(arrN);
+		
 		int M = Integer.parseInt(br.readLine());
+		
 		stringTokenizer = new StringTokenizer(br.readLine());
 		for (int i = 0; i < M; i++) {
+			int tar = Integer.parseInt(stringTokenizer.nextToken());
 			
-			int number = Integer.parseInt(stringTokenizer.nextToken());
-			Integer tar = map.get(number);
+			int stIdx = lower_idx(arrN, tar);
+			int enIdx = upper_idx(arrN, tar);
 			
-			if(null == tar) {
-				sb.append("0 ");
-			}else {
-				sb.append(String.valueOf(tar)+" ");
-			}
-			
+			sb.append(enIdx-stIdx + " ");
 		}
+		
+		
 		System.out.println(sb.toString().trim());
+		
+	}
+
+	private static int lower_idx(int[] arr, int target) {
+		
+		int st = 0;
+		int en = arr.length;
+		
+		while (st < en) {
+			int mid = (st+en)/2;
+			if(arr[mid] >= target) {
+				en = mid;
+			}else {
+				st = mid+1;
+			}
+		}
+		return st;
+	}
+	
+	private static int upper_idx(int[] arr, int target) {
+		
+		int st = 0;
+		int en = arr.length;
+		
+		while (st < en) {
+			int mid = (st+en)/2;
+			if(arr[mid] > target) {
+				en = mid;
+			}else {
+				st = mid +1;
+			}
+		}
+		return st;
 	}
 }
