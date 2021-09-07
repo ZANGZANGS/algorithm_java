75b01fac8e7ff987e79e9ea50b58006aed12ab71
ZANGZANGS 49123158+ZANGZANGS@users.noreply.github.com <kimjangh94@gmail.com>
2021-09-07 23:49:57 +0900

    N 과 M (1) 다시 풀기

--- algorithm_java/backtracking/BOJ_15649.java
+++ algorithm_java/backtracking/BOJ_15649.java
@@ -3,105 +3,61 @@ package backtracking;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
+/**
+ * @source		: baekjoon
+ * @algorithm	: backtracking
+ * @description	: N과 M (1)
+ * ==============================================
+ * DATE			NOTE	
+ * ==============================================
+ * 2021.09.07	다시 풀자..
+ */
 public class BOJ_15649 {
 	
-	static int N;
-	static int M;
-	static boolean[] isUsed; 
+	static int N,M;
 	static int[] arr;
+	static boolean[] isused;
 	static StringBuilder sb;
 	
 	public static void main(String[] args) throws IOException{
+		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String input = br.readLine();
-		StringTokenizer st = new StringTokenizer(input, " ");
-		
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		arr = new int[M];
-		isUsed = new boolean[N+1];
-
 		sb = new StringBuilder();
-		func(0);
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		
+		N = Integer.parseInt(st.nextToken()); //숫자 까지~
+		M = Integer.parseInt(st.nextToken());
+		arr = new int[M]; // 배열 담을 갯수
+		isused = new boolean[10]; // 숫자 사용 여부
+		
+		
+		func(0);
 		System.out.println(sb.toString());
+		
 	}
 	
-	static void func(int step) {
+	public static void func(int k){
 		
-		if(step == M) {
-			Arrays.stream(arr).forEach(v-> sb.append(v).append(" "));
+		if(k == M) {
+			for (int i = 0; i < M; i++) {
+				sb.append(arr[i]+" ");
+			}
 			sb.append("\n");
 			return;
 		}
 		
-		
-		for (int i = 1; i <= N; i++) {
-			
-			if(!isUsed[i]) {
-				isUsed[i] = true;
-				arr[step] = i;
-				func(step+1);
-				isUsed[i] = false;
+		for (int i = 1; i <= N; i++) {// 1 ~ N
+			if(!isused[i]) {
+				arr[k] = i;
+				isused[i] = true;
+				func(k+1);
+				isused[i] = false;
 			}
 			
 		}
+		
+		
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-//	static int n,m;
-//	static int[] arr;
-//	static boolean[] isused;
-//	static StringBuilder sb;
-//	
-//	public static void main(String[] args) throws IOException{
-//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//		String input = br.readLine();
-//		StringTokenizer st = new StringTokenizer(input, " ");
-//		n = Integer.parseInt(st.nextToken());
-//		m = Integer.parseInt(st.nextToken());
-//		
-//		arr = new int[m];
-//		isused = new boolean[n+1]; // +1 �� �ϴ� ������ �迭 �ε����� 0 ���� �����ؼ�
-//		sb= new StringBuilder();
-//		
-//		func(0);
-//		System.out.print(sb.toString());
-//	}
-//
-//	static void func(int k) {
-//		
-//		if(k == m) { // m���� ��� ���ߴٸ�
-//			for(int i = 0 ; i< m ; i++) {
-//				sb.append(arr[i]+" "); // arr�� ����ص� ���� ���
-//			}
-//			sb.append("\n");
-//			return;
-//		}
-//		
-//		for(int i = 1; i <= n; i++) { // ���� �״�� ����Ұ���
-//			if(!isused[i]) {// ���� i �� ������ �ʾ�����
-//				arr[k] = i; // k��° ���� i�� ����
-//				isused[i] = true; // i�� ����ߴٰ� ǥ��
-//				func(k+1); // ���� ���� ���Ϸ� �� �ܰ� �� ��
-//				isused[i] = false;	// k��° ���� i�� �� ��� ��츦 Ȯ�������� ������� �������� ����
-//			}
-//		}
-//	}
 }
