9549bd9fe587c7f710b54c37e2c5ce0c342835e7
ZANGZANGS 49123158+ZANGZANGS@users.noreply.github.com <kimjangh94@gmail.com>
2021-09-09 13:01:04 +0900

    Z 재귀

--- algorithm_java/recursion/BOJ_1074.java
+++ algorithm_java/recursion/BOJ_1074.java
@@ -1,6 +1,3 @@
-/**
- * Z ∑Œ πÊπÆ«œ±‚
- */
 package recursion;
 
 import java.io.BufferedReader;
@@ -8,31 +5,46 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+/**
+ * @source		: beakjoon
+ * @algorithm	: recursion
+ * @description	: Z
+ * ==============================================
+ * DATE			NOTE	
+ * ==============================================
+ * 2021.09.09
+ */
 public class BOJ_1074 {
 	
+	
 	public static void main(String[] args) throws IOException {
+	
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String input = br.readLine();
-		StringTokenizer st = new StringTokenizer(input, " ");
 		
-		int N = Integer.parseInt(st.nextToken());
-		int r = Integer.parseInt(st.nextToken());
-		int c = Integer.parseInt(st.nextToken());
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		System.out.println(zVisit(N, r, c));
+		int N = Integer.parseInt(st.nextToken()); 
+		int r = Integer.parseInt(st.nextToken()); 
+		int c = Integer.parseInt(st.nextToken()); 
+		
+		System.out.println(func(N,r,c));
 	}
 	
-	public static int zVisit(int N, int r, int c) {
-		
-		if(N == 0){
+	private static int func(int n, int r, int c) {
+	
+		if(n ==0) {
 			return 0;
-		} else {
-			int dist = 1<<(N-1);	// ¡¬«•∏¶ øÚ¡˜¿œ ∞≈∏Æ
-			
-			if(r<dist && c<dist) return zVisit(N-1, r, c);								//1 
-			else if(r<dist && c>=dist) return (dist*dist) + zVisit(N-1, r, c-dist);		//2
-			else if(r>=dist && c<dist) return (dist*dist)*2 + zVisit(N-1, r-dist, c);	//3
-			else return (dist*dist)*3 + zVisit(N-1, r-dist, c-dist);					//4
+		}
+		int half = 1 << (n-1);
+		
+		if( c < half && r < half) {
+			return func(n-1, r, c); //1Î≤à ÏÇ¨Í∞ÅÌòï
+		}else if( c >= half && r < half) {
+			return half*half + func(n-1, r, c-half); //2Î≤à ÏÇ¨Í∞ÅÌòï
+		}else if( c < half && r >= half) {
+			return 2*half*half + func(n-1, r-half, c); //3Î≤à ÏÇ¨Í∞ÅÌòï
+		}else {
+			return 3*half*half + func(n-1, r-half, c-half); //4Î≤à ÏÇ¨Í∞ÅÌòï
 		}
 	}
 }
@@ -40,77 +52,3 @@ public class BOJ_1074 {
 
 
 
-//package recursion;
-//
-//import java.io.BufferedReader;
-//import java.io.IOException;
-//import java.io.InputStreamReader;
-//import java.util.StringTokenizer;
-//
-//public class BOJ_1074 {
-//
-//	static int count;
-//	static int r;
-//	static int c;
-//	static boolean isFind;
-//	public static void main(String[] args) throws IOException {
-//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//		String input = br.readLine();
-//		StringTokenizer st = new StringTokenizer(input, " ");
-//		
-//		int N = Integer.parseInt(st.nextToken());
-//		r = Integer.parseInt(st.nextToken());
-//		c = Integer.parseInt(st.nextToken());
-//		
-//		int dot = 1<<(N-1);
-//		goZ(N, dot, dot);
-//		
-//		//System.out.println("COUNT: "+count);
-//	}
-//	
-////		(1<<n * 1<<n)/4		z±◊∏Æ±‚ »Ωºˆ
-////		n=1  2*2=4			1			1,1
-////		n=2  4*4=16  		4			2,2
-////		n=3  8*8=64  		16			4,4
-////		n=4  16*16= 		64			8,8	
-//	
-//	//n π¯¬∞ø° 
-//	public static void goZ(int n, int rDot, int cDot) {
-//		if(!isFind) {
-//			if(n == 1) {
-//				zMove(rDot, cDot);			//4, 4
-//			} else {
-//				int moveDot = 1<<(n-2);	//ªÛ«œ¡¬øÏ∑Œ øÚ¡˜¿Ã¥¬ ƒ≠ºˆ
-//				
-//				goZ(n-1, rDot-moveDot, cDot-moveDot);	// 2 2
-//				goZ(n-1, rDot-moveDot, cDot+moveDot);	// 2 6
-//				goZ(n-1, rDot+moveDot, cDot-moveDot);	// 6 2
-//				goZ(n-1, rDot+moveDot, cDot+moveDot);	// 6 6
-//			}
-//		}
-//			
-//		
-//	}
-//	public static void zMove(int rDot, int cDot) {
-//		if(r == rDot-1 && c == cDot-1) {
-//			System.out.println(count); 
-//			isFind = true;
-//		}
-//		count++;
-//		if(r == rDot-1 && c == cDot) {
-//			System.out.println(count); 
-//			isFind = true;
-//		}
-//		count++;
-//		if(r == rDot && c == cDot-1)  {
-//			System.out.println(count); 
-//			isFind = true;
-//		}
-//		count++;
-//		if(r == rDot && c == cDot) {
-//			System.out.println(count); 
-//			isFind = true;
-//		}
-//		count++;
-//	}
-//}
