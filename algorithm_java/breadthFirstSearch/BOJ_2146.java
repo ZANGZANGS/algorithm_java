af725a044c229410999194277badcb69f4999762
ZANGZANGS 49123158+ZANGZANGS@users.noreply.github.com <kimjangh94@gmail.com>
2021-09-13 11:14:44 +0900

    다리만들기 재풀이

--- algorithm_java/breadthFirstSearch/BOJ_2146.java
+++ algorithm_java/breadthFirstSearch/BOJ_2146.java
@@ -7,132 +7,138 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
+/**
+ * @source		: baekjoon
+ * @algorithm	: bfs
+ * @description	: 다리만들기
+ * ==============================================
+ * DATE			NOTE	
+ * ==============================================
+ * 2021.09.13
+ */
 public class BOJ_2146 {
 
-	static int n;
+	static int N;
 	static int[][] map;
 	static int[][] island;
-	static int[][] dist;
+	static int ord;
+
 	static int[] dx = {0,1,0,-1};
 	static int[] dy = {1,0,-1,0};
-	static Queue<int[]> queue;
 	
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		
-		n = Integer.parseInt(br.readLine());
-		map = new int[n][n];
-		dist = new int[n][n];
-		island = new int[n][n];
-		queue = new LinkedList<int[]>();
+		N = Integer.parseInt(br.readLine());
 		
-		int minDist = Integer.MAX_VALUE;
+		map = new int[N][N];
+		island = new int[N][N];
 		
-		for (int i = 0; i < n; i++) {
+		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < n; j++) {
-				int tmp = Integer.parseInt(st.nextToken());
-				map[i][j] = tmp; 
+			
+			for (int j = 0; j < N; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
 		
-		//1. 섬 개수 구하기
-		findIslandBfs();
-		//2. 각 섬으로부터의 거리 구하기
-		calDistBfs();
+		findIsland();
 		
-		//3. island와 dist 으로 정답 구해보자
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < n; j++) {
-				for (int k = 0; k < 4; k++) {
-					int nx = j+dx[k];
-					int ny = i+dy[k];
-					
-					//상하 좌우 범위 확인
-					if(nx >= n || ny >= n || 0 > nx || 0 > ny) continue;
-					if(island[ny][nx] != island[i][j]) {
-						minDist= Math.min(minDist , dist[i][j]+dist[ny][nx]);
-						
-					}
-					
-				}
-			}
+		int result = Integer.MAX_VALUE;
+		
+		for (int i = 1; i <= ord; i++) {
+			result = Math.min(result, calBridge(i));
 		}
-		
-		System.out.println(minDist);
+		System.out.println(result);
 		
 	}
 	
-	public static void findIslandBfs() {
-		queue.clear();
-		int islandNumber = 0;
+	private static void findIsland() {
 		
-		for (int i = 0; i < n; i++) {//y
-			for (int j = 0; j < n; j++) {//x
+		Queue<int[]> Q = new LinkedList<int[]>();
+		
+		boolean[][] vis = new boolean[N][N];
+		
+		ord = 0;
+		
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
 				
-				if(map[i][j] == 1 && island[i][j] == 0) {
-					island[i][j] = ++islandNumber; //섬 번호 증가
-					queue.add(new int[] {j,i});
+				if(map[i][j] != 1) continue;
+				if(vis[i][j]) continue;
+				
+				Q.add(new int[] {i,j}); // y,x
+				vis[i][j] = true;
+				island[i][j] = ++ord;
+				
+				while (!Q.isEmpty()) {
+					int[] temp = Q.poll();
 					
-					while (!queue.isEmpty()) {
-						int[] dot = queue.poll();
-						int x = dot[0];
-						int y = dot[1];
+					int x = temp[1];
+					int y = temp[0];
+					for (int k = 0; k < 4; k++) {
+						int nx = x + dx[k];
+						int ny = y + dy[k];
 						
-						// 상하좌우 좌표 체크
-						for (int k = 0; k < 4; k++) {
-							int nx = x + dx[k];
-							int ny = y + dy[k];
-							
-							//좌표 범위를 벗어나는지 체크
-							if(nx >= n || ny >= n || 0 > nx || 0 > ny) continue;
-							// 섬이 아닌지 체크
-							if(map[ny][nx] == 0) continue;
-							// 이미 지나간 곳인지 체크
-							if(island[ny][nx] > 0) continue;
-							queue.add(new int[] {nx,ny});
-							island[ny][nx] = islandNumber;
-							
-						}
+						if(nx<0 || ny< 0 || nx>=N || ny>= N) continue;
+						
+						if(vis[ny][nx]) continue;
+						if(map[ny][nx] == 0) continue;
+						
+						island[ny][nx] = ord;
+						Q.add(new int[] {ny, nx});
+						vis[ny][nx] = true;
+						
 					}
 				}
 			}
 		}
 	}
 	
-	static void calDistBfs() {
-        queue.clear();
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < n; j++) {
-				if(map[i][j] == 1) queue.add(new int[] {j,i});//섬을 다 큐에 넣는다
-				else dist[i][j] = -1;
+	private static int calBridge(int order) {
+		
+		Queue<int[]> Q = new LinkedList<int[]>();
+		int[][] dist = new int[N][N];
+		
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				if(island[i][j] != order) continue;
+				if(map[i][j] != 1) continue;
+
+				Q.add(new int[] {i,j});	//y,x
+				dist[i][j] = 0;
+				
 			}
 		}
 		
-		while (!queue.isEmpty()) {
-			int[] dot = queue.poll();
-			int x = dot[0];
-			int y = dot[1];
+		while (!Q.isEmpty()) {
+			int[] temp = Q.poll();
+			
+			int x = temp[1];
+			int y = temp[0];
 			
 			for (int k = 0; k < 4; k++) {
 				int nx = x + dx[k];
 				int ny = y + dy[k];
 				
-				//상하좌우를 벗어나는지 확인
-				if(nx<0 || ny <0 || nx >=n || ny >=n) continue;
-				//이미 거리를 지나간 곳은 pass
-				if(dist[ny][nx] != -1)continue;
+				if(nx<0 || ny< 0 || nx>=N || ny>= N) continue;
 				
-				dist[ny][nx] = dist[y][x]+1; //거리 증가
-				island[ny][nx] = island[y][x]; //섬의 범위 증가 why? dist가 어느 섬으로부터의 거리인지 확인하기 위해서
-				queue.add(new int[] {nx,ny});
+				if(dist[ny][nx] >0) continue; //중복방문 체크
+				
+				if(island[ny][nx] != 0 && island[ny][nx] !=order) {
+					//물도 아니고, 다른 대륙을 밟으면, 최단거리 대륙 찾은 것
+					return dist[y][x];
+				}
+				if(map[ny][nx] == 1) continue;
+				
+				Q.add(new int[] {ny, nx});
+				dist[ny][nx] = dist[y][x] +1;
 				
 			}
 		}
 		
+		return -1;
 	}
 	
-
-
 }
