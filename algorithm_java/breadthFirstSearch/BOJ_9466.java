06d0556fbd57d55700d3a832f1521902a91a7de3
ZANGZANGS 49123158+ZANGZANGS@users.noreply.github.com <kimjangh94@gmail.com>
2021-09-13 11:15:46 +0900

    팀 프로젝트 다시풀기,,

--- algorithm_java/breadthFirstSearch/BOJ_9466.java
+++ algorithm_java/breadthFirstSearch/BOJ_9466.java
@@ -5,62 +5,68 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+/**
+ * @source		: baekjoon
+ * @algorithm	: bfs
+ * @description	: 팀 프로젝트
+ * ==============================================
+ * DATE			NOTE	
+ * ==============================================
+ * 2021.09.10 TODO 다시 풀자
+ */
 public class BOJ_9466 {
-
-	static int[] arr;
-	static boolean[] vis;
-	static boolean[] done;
-	static int n;
-	static int result;
 	
+	static int n;
+	static int arr[];
+	static boolean vis[];
+	static boolean done[];
+	static int result;
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		int t = Integer.parseInt(br.readLine());
+		int T = Integer.parseInt(br.readLine());
 		
-		for (int i = 0; i < t; i++) {
+		for (int i = 0; i < T; i++) {
+			n = Integer.parseInt(br.readLine()); // 2 ~ 100,000
 			
-			n = Integer.parseInt(br.readLine());
 			arr = new int[n+1];
 			vis = new boolean[n+1];
 			done = new boolean[n+1];
-			result=0;
-			
+			result = 0;
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			
 			for (int j = 1; j <= n; j++) {
 				arr[j] = Integer.parseInt(st.nextToken());
 			}
 			
+			
 			for (int j = 1; j <= n; j++) {
-				if(done[j]) continue;
-				bfs(j);
+				if(done[j]) continue; //팀 구성 완료인 경우
+				func(j);
 			}
+			
 			
 			System.out.println(n-result);
 			
 		}
-				
-		
 	}
-	static void bfs(int n) {
+	
+	private static void func(int cur) {
 		
-		if(vis[n]) {
-			//이미 방문한 곳이면
-			done[n] = true;
-			result++;
+		if(vis[cur]) {
+			done[cur] = true;
+			result ++;
 		}else {
-			vis[n]=true;
+			vis[cur] = true;
 		}
 		
-		int next = arr[n];
+		int next = arr[cur];
+		if(!done[next]) func(next);
 		
-		if(!done[next]) {
-			bfs(next);
-		}
+		vis[cur] = false;
+		done[cur] = true;
 		
-		vis[n] = false;
-		done[n] = true;
-		
+		return;
 	}
 }
