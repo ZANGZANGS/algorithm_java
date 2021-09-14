3f31ed75c2f70a47106604dce179417adc99059d
ZANGZANGS 49123158+ZANGZANGS@users.noreply.github.com <kimjangh94@gmail.com>
2021-09-14 13:25:03 +0900

    숨바꼭질 우선순위 큐 풀이

--- algorithm_java/breadthFirstSearch/BOJ_13549.java
+++ algorithm_java/breadthFirstSearch/BOJ_13549.java
@@ -4,8 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 
@@ -17,6 +16,7 @@ import java.util.StringTokenizer;
  * DATE			NOTE	
  * ==============================================
  * 2021.09.14	순간이동은 시간이 0초, 먼저 0초일 때 이동 다시킴.
+ * 2021.09.14	우선순위 큐 풀이
  */
 public class BOJ_13549 {
 
@@ -32,32 +32,36 @@ public class BOJ_13549 {
 		int[] arr = new int[100001];
 		Arrays.fill(arr, -1);
 		
-		Queue<Integer> Q = new LinkedList<Integer>();
+		PriorityQueue<int[]> Q = new PriorityQueue<>((a,b)-> a[1]-b[1]);
 		
-		Q.add(N);
+		
+		Q.add(new int[] {N,0});
 		arr[N] = 0;
 
 		while (!Q.isEmpty()) {
 			
-			int cur =Q.poll();
+			int[] temp =Q.poll();
+			int cur = temp[0];
+			int dist = temp[1];
 		
-			if(2*cur <= 100000 && arr[2*cur] == -1) {
-				for (int i = cur; i < arr.length; i=2*i) {
-					if(arr[i] != -1) continue;
-					
-					arr[i] = arr[cur];
-					Q.add(i);
+			//순간이동
+			if( 2*cur <= 100000) {
+				
+				if(arr[2*cur] == -1) {
+					arr[2*cur] = dist;
+					Q.add(new int[] {2*cur,dist});
+				}else if(arr[2*cur] > dist) {
+					arr[2*cur] = dist;
+					Q.add(new int[] {2*cur,dist});
 				}
 			}
 			
 
-
 			for (int tar : new int[] {cur-1, cur +1}) {
 				if(tar< 0 || tar > 100000) continue;
 				if(arr[tar] != -1) continue;
-				
-				arr[tar] = arr[cur]+1;
-				Q.add(tar);
+				arr[tar] = dist+1;
+				Q.add(new int[] {tar, dist+1});
 			}
 			
 		}
