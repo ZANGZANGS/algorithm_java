87dfc58c8c62398208837a5cc9c0b9045bece1f9
ZANGZANGS 49123158+ZANGZANGS@users.noreply.github.com <kimjangh94@gmail.com>
2021-09-24 10:41:47 +0900

    곱셈 재풀이 성공

--- algorithm_java/recursion/BOJ_1629.java
+++ algorithm_java/recursion/BOJ_1629.java
@@ -12,7 +12,8 @@ import java.util.StringTokenizer;
  * ==============================================
  * DATE			NOTE	
  * ==============================================
- * 2021.08.24	TODO 다시 풀기
+ * 2021.08.24	다시 풀기
+ * 2021.09.24	성공!
  */
 public class BOJ_1629 {
 
@@ -23,9 +24,28 @@ public class BOJ_1629 {
 		String input = br.readLine();
 		StringTokenizer st = new StringTokenizer(input);
 		
-		long a = Integer.parseInt(st.nextToken());
-		long b = Integer.parseInt(st.nextToken());
-		long c = Integer.parseInt(st.nextToken());
+		
+		long a = Long.parseLong(st.nextToken());
+		long b = Long.parseLong(st.nextToken());
+		long c = Long.parseLong(st.nextToken());
+		
+		System.out.println(func(a,b,c));
+	}
+	
+	private static long func(long a, long b, long c) {
+		if(b==1) {
+			return a%c;
+		}
+
+		long val = func(a,b/2,c);
+		
+		if(b%2 == 0) {
+			return val * val % c;
+			
+		}else {
+			return (val * val % c) * a % c;
+		}
+		
 		
 	}
 	
