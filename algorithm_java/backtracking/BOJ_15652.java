79baf14592ad4680a0a53182a6bd3099850f102d
ZANGZANGS 49123158+ZANGZANGS@users.noreply.github.com <kimjangh94@gmail.com>
2021-10-12 09:37:26 +0900

    N과 M (4) 성공

---
+++ algorithm_java/backtracking/BOJ_15652.java
@@ -0,0 +1,56 @@
+package backtracking;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * @source		: baekjoon
+ * @algorithm	: backtracking
+ * @description	: N &M (3)
+ * ==============================================
+ * DATE			NOTE	
+ * ==============================================
+ * 2021.10.12	성공
+ */
+public class BOJ_15652 {
+	static int N,M;
+	static StringBuilder sb;
+	static int[] arr;
+	public static void main(String[] args) throws IOException{
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		sb = new StringBuilder();
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		arr = new int[M];
+		func(0);
+		
+		System.out.println(sb.toString());
+	}
+	
+	private static void func(int k) {
+		if(k == M) {
+			for (int i = 0; i < k; i++) {
+				sb.append(arr[i]);
+				sb.append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+		
+		for (int i = 1; i <= N; i++) {
+			
+			if(k > 0 && arr[k-1] > i) {
+				continue;
+			}
+			
+			arr[k] = i;
+			func(k+1);
+		}
+	}
+
+}
