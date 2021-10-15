bc1ef8a38a07790ed50990a9db9b5bb5f4f4f432
ZANGZANGS 49123158+ZANGZANGS@users.noreply.github.com <kimjangh94@gmail.com>
2021-10-15 13:21:14 +0900

    N& M (12) 성공

---
+++ algorithm_java/backtracking/BOJ_15666.java
@@ -0,0 +1,78 @@
+package backtracking;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+/**
+* @source		: baekjoon
+* @algorithm	: backtracking
+* @description	: N& M (12)
+* ==============================================
+* DATE			NOTE	
+* ==============================================
+* 2021.10.15	
+*/
+public class BOJ_15666 {
+
+	static int N,M;
+	static int[] numbers;
+	static int[] arr;
+	static StringBuilder sb;
+	
+	public static void main(String[] args) throws IOException{
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		sb = new StringBuilder();
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		numbers = new int[N];
+		arr = new int[M];
+		
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			numbers[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(numbers);
+		
+		func(0);
+		
+		System.out.println(sb.toString());
+		
+	}
+	
+	private static void func(int k) {
+		if(k == M) {
+			for (int i = 0; i < k; i++) {
+				sb.append(arr[i]);
+				sb.append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+		
+		int previous = -1;
+		
+		for (int i = 0; i < N; i++) {
+			
+			if(k>0 && arr[k-1] > numbers[i]) {
+				continue;
+			}
+			
+			if(previous != numbers[i] ) {
+				
+				previous = numbers[i];
+				arr[k] = numbers[i];
+				func(k+1);
+			}
+			
+		}
+	}
+
+}
