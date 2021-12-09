ea3a1b3084e69c7efd5837ae0cb380a40d7b9d96
KIMJANGHYEON <kimjangh94@gmail.com>
2021-12-09 23:18:11 +0900

    시간초과

--- algorithm_java/simulation/BOJ_14500.java
+++ algorithm_java/simulation/BOJ_14500.java
@@ -3,7 +3,9 @@ package simulation;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Stack;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.StringTokenizer;
 /**
  * @source		: baekjoon
@@ -13,10 +15,11 @@ import java.util.StringTokenizer;
  * DATE			NOTE	
  * ==============================================
  * 2021.12.08	실패.. ㅜ_ㅜ
+ * 2021.12.20	시간초과
  */
 public class BOJ_14500 {
 
-	public static void main(String[] args) throws IOException{
+public static void main(String[] args) throws IOException{
 		
 		final int[] dx = new int[] {0,1,0,-1};
 		final int[] dy = new int[] {1,0,-1,0};
@@ -30,71 +33,74 @@ public class BOJ_14500 {
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
 		
-		int[][] paper = new int[N][M];
+		int[][] papper = new int[N][M];
 		
-		int maxVal = 0;
 		
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < M; j++) {
-				paper[i][j] = Integer.parseInt(st.nextToken());
-				maxVal = Math.max(maxVal, paper[i][j]);
+				papper[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
 		
-		for (int lv = maxVal; lv > 0; lv--) {
-			boolean[][] temp = new boolean[N][M];
-			
-			for (int i = 0; i < N; i++) {
-				for (int j = 0; j < M; j++) {
-					if(paper[i][j]>=lv) temp[i][j] = true;
-				}
-			}
-			
-
-			//순회 start
-			for (int i = 0; i < N; i++) {
-				for (int j = 0; j < M; j++) {
+		int maxVal = 0;
+		//순회 start
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				
+				List<int[]> tetromino = new ArrayList<>();
+				
+				
+				tetromino.add(new int[] {i,j,papper[i][j]});
+				boolean[][] isUsed = new boolean[N][M];
+				isUsed[i][j] = true;
+				
+				List<int[]> next = new ArrayList<>(); //다음 갈 수 있는 좌표들	
+				
+				
+				for (int idx = 0; idx < 3; idx++) {
+					int[] cur  =tetromino.get(idx);
 					
-					if(!temp[i][j]) continue;
-					//DFS
-					Stack<int[]> stack = new Stack<>();
-					
-					
-					stack.add(new int[] {i,j});
-					int[][] dist = new int[N][M];
-					dist[i][j] = 1;
-					
-					while(!stack.isEmpty()) {
-						int[] cur = stack.pop();
+					for (int k = 0; k < 4; k++) {
+						int ny =cur[0]+dy[k];
+						int nx =cur[1]+dx[k];
 						
-						int y = cur[0];
-						int x = cur[1];
+						if(nx<0 || ny<0 || nx>=M || ny>=N) continue;
+						if(isUsed[ny][nx]) continue;
 						
-						for (int k = 0; k < 4; k++) {
-							int nx = x + dx[k];
-							int ny = x + dy[k];
-							
-							if(nx<0 || ny<0 || nx>=M || ny>=N) continue;
-							
-							if(dist[ny][nx]>0) continue;
-							if(!temp[ny][nx]) continue;
-							
-							stack.add(new int[] {ny,nx});
-							dist[ny][nx] = dist[y][x]+1;
-							
-							if(dist[ny][nx] == 4) {
-								//테트리스 반단하기
-								System.out.println("");
-							}
-						}
+						next.add(new int[] {ny,nx,papper[ny][nx]});
 						
 					}
 					
+					
+					if(next.isEmpty()) {
+						break;
+					}
+					
+					//테트로미노 추가
+					Collections.sort(next, (a,b)->b[2]-a[2]); 
+					int[] maxDot = next.remove(0);
+					tetromino.add(maxDot);
+					isUsed[maxDot[0]][maxDot[1]] = true;
+					
+					
 				}
-			}//순회 end
-		}
+				
+				
+				if(tetromino.size() == 4) {
+					int sum = 0;
+					for (int[] tet : tetromino) {
+						sum +=tet[2];
+					}
+					
+					maxVal = Math.max(maxVal, sum);
+				}
+				
+				
+			}
+		}//순회 end
 		
+		System.out.println(maxVal);
 
 	}
 
