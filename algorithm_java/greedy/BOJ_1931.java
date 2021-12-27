bfc8741768eb56f5a752681e690a30cd6893fe16
KIMJANGHYEON <kimjangh94@gmail.com>
2021-12-27 12:01:36 +0900

    회의실배정 그리디 재풀이

--- algorithm_java/greedy/BOJ_1931.java
+++ algorithm_java/greedy/BOJ_1931.java
@@ -4,20 +4,28 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Comparator;
+import java.util.Collections;
 import java.util.List;
 import java.util.StringTokenizer;
-import java.util.stream.Collectors;
-
+/**
+ * @source		: 백준
+ * @algorithm	: 그리디
+ * @description	: 회의실배정
+ * ==============================================
+ * DATE			NOTE	
+ * ==============================================
+ * 2021.12.27	재풀이
+ */
 public class BOJ_1931 {
 
 	public static void main(String[] args) throws IOException{
 		BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
 		
 		int n = Integer.parseInt(br.readLine());	//회의의 수
+		List<int[]> list = new ArrayList<>();
+		int count = 0; //스케줄 개수 
 		
-		List<int[]> list = new ArrayList<int[]>();
-		StringTokenizer st;
 		for (int i = 0; i < n; i++) {
 			st = new StringTokenizer(br.readLine());
 			int s = Integer.parseInt(st.nextToken());
@@ -25,34 +33,23 @@ public class BOJ_1931 {
 			
 			list.add(new int[] {s,e});
 		}
-		Comparator<int[]> comparator = new Comparator<int[]>() {
-			
-			@Override
-			public int compare(int[] o1, int[] o2) {
-				if(o1[1] == o2[1]) {
-					return  o1[0] - o2[0]; //틀린이유: 동일 종료 시간일 경우 시작시간 오름차순 정렬을 생각하지 않음
-				}
-				return  o1[1] - o2[1];
-			}
-			
-		};
 
-		// 종료 시간 기준으로 정렬
-		list = list.stream()
-				.sorted(comparator)
-				.collect(Collectors.toList());
-		
-		int time = 0;//현재 시간
-		int ans = 0; //스켸줄 개수
-		//그리디~
-		for (int i = 0; i < n; i++) {
+		Collections.sort(list, (a,b)->{
+			if(a[1]==b[1]) {
+				return a[0]-b[0];
+			}
+			return a[1]-b[1];
 			
-			if(time> list.get(i)[0]) continue;	//시작시간
-			time = list.get(i)[1];//종료시간
-			ans ++;
+		});
+		
+		int now = 0;//현재 시간
+		for (int i = 0; i < n; i++) {
+			if(now> list.get(i)[0]) continue;	//시작시간 체크
+			now = list.get(i)[1]; //선택한 회의의 종료시간으로 변경
+			count++;
 		}
 		
-		System.out.println(ans);
+		System.out.println(count);
 	}
 
 }
