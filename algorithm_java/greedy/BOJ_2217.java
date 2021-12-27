93b01a0c896b0fb8d2b04dc452829ea0831757f4
KIMJANGHYEON <kimjangh94@gmail.com>
2021-12-27 12:01:53 +0900

    로프 그리디 재풀이

--- algorithm_java/greedy/BOJ_2217.java
+++ algorithm_java/greedy/BOJ_2217.java
@@ -5,38 +5,34 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Collections;
-
+/**
+ * @source		: 백준
+ * @algorithm	: 그리디
+ * @description	: 로프
+ * ==============================================
+ * DATE			NOTE	
+ * ==============================================
+ * 2021.12.27	재풀이
+ */
 public class BOJ_2217 {
 
 	public static void main(String[] args) throws IOException{
 		BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());	
+		Integer[] rope = new Integer[N];
+		int[] weight = new int[N]; 	//무게 저장 배열
 		
-		int n = Integer.parseInt(br.readLine());	//로프 개수
-		
-		Integer[] arr = new Integer[n];
-		
-		for (int i = 0; i < arr.length; i++) {
-			arr[i] = Integer.parseInt(br.readLine());	//로프 중량 정보
+		for (int i = 0; i < N; i++) {
+			rope[i] = Integer.parseInt(br.readLine());	//로프 중량 정보
 		}
 		
-		//로프 정보 정렬
-        Arrays.sort(arr, Collections.reverseOrder());
+        Arrays.sort(rope, Collections.reverseOrder());
 		
-		int maxWeight = 0;
-		int[] w = new int[n]; //누적 합 저장
-		
-		for (int i = 0; i < arr.length; i++) {
-			w[i] += arr[i];
-			int k = i+1; //사용하는 밧줄 개수
-			int temp = w[i]/k; //w/k 의 무게 계산
-			
-			//분산된 무게가 최저 밧줄보다 크다면 들 수 없다. 밧줄이 끊어질 것이다. 다음으로 넘어간다.
-			if(temp >w[i]) continue;
-			//현재 밧줄의 무게 *k             
-			maxWeight = Math.max(maxWeight, arr[i] * k);
+		for (int i = 0; i < N; i++) {
+			weight[i] = rope[i]*(i+1);
 		}
 		
-		System.out.println(maxWeight);
+		System.out.println(Arrays.stream(weight).max().getAsInt());
 
 	}
 
