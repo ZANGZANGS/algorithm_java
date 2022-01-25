e30363bf98f9b799425d2c0309cf8a3fd5eaa8f4
zangzangs <kimjangh94@gmail.com>
2022-01-25 12:44:29 +0900

    나무 재테크 시뮬레이션 성공

--- algorithm_java/simulation/BOJ_16235.java
+++ algorithm_java/simulation/BOJ_16235.java
@@ -3,11 +3,12 @@ package simulation;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
 import java.util.StringTokenizer;
+
 /**
  * @source		: 백준
  * @algorithm	: 시물레이션
@@ -16,6 +17,7 @@ import java.util.StringTokenizer;
  * DATE			NOTE	
  * ==============================================
  * 2022.01.25	시간초과
+ * 2022.01.25	성공 (우선순위 큐를 활용하여 시간 단축)
  */
 public class BOJ_16235 {
 
@@ -39,20 +41,16 @@ public class BOJ_16235 {
 			}
 		}
 		
-		List<Integer>[][] tree  = new ArrayList[N][N];
-		
+//		List<Integer>[][] tree  = new ArrayList[N][N];
+		PriorityQueue<int[]> pQ = new PriorityQueue<>((a,b)-> a[2]-b[2]); //나이가 적은 순으로 정렬
 		//상도가 심은 나무의 정보
 		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
 			int x = Integer.parseInt(st.nextToken())-1;//위에서 떨어진 칸 
 			int y = Integer.parseInt(st.nextToken())-1;//왼쪽에서 떨어진 칸
 			int z = Integer.parseInt(st.nextToken()); //나이
-			
-			if(tree[x][y] == null) {
-				tree[x][y] = new ArrayList<Integer>();
-			}
-			
-			tree[x][y].add(z);
+		
+			pQ.add(new int[] {x,y,z});
 		}
 		
 		//처음 땅에는 양분이 5씩 들어있다.
@@ -64,64 +62,57 @@ public class BOJ_16235 {
 		int year = 0;
 		while(year < K) {
 			//봄, 여름
-			for (int i = 0; i < N; i++) {
-				for (int j = 0; j < N; j++) {
-					
-					if(tree[i][j] == null || tree[i][j].size() == 0 ) {
-						continue;
-					}
-					
-					List<Integer> now = tree[i][j];
-					Collections.sort(now); //나이순으로 정렬
-					List<Integer> after = new ArrayList<Integer>();
-					
-					//봄
-					while(!now.isEmpty()) {
-						if(now.get(0)>map[i][j]) break;
-						
-						int age = now.remove(0);
-						map[i][j] -= age;
-						after.add(age+1); //1살 자란 나무
-					}
-
-					tree[i][j] = after;
-					
-					//여름
-					while(!now.isEmpty()) {
-						map[i][j] += now.remove(0)/2;
-					}
-					
+			Queue<int[]> summerQ = new LinkedList<>(); 
+			Queue<int[]> nextQ = new LinkedList<>(); 
+			while (!pQ.isEmpty()) {
+				int[] cur = pQ.poll();
+				int r = cur[0];
+				int c = cur[1];
+				int age = cur[2];
+				
+				if(map[r][c] < age) {
+					summerQ.add(cur);
+				}else {
+					map[r][c] -= age;
+					nextQ.add(new int[] {r,c,age+1});
 				}
+				
+			}
+			//여름
+			while(!summerQ.isEmpty()) {
+				int[] cur = summerQ.poll();
+				int r = cur[0];
+				int c = cur[1];
+				int age = cur[2];
+				
+				map[r][c] += age/2;
 			}
 			
+			//가을
+			while(!nextQ.isEmpty()) {
+				int[] cur = nextQ.poll();
+				int r = cur[0];
+				int c = cur[1];
+				int age = cur[2];
+
+				pQ.add(cur);
+				
+				//번식
+				if(age%5 == 0) {
+					for (int k = 0; k < 8; k++) {
+						int nr = r + dr[k];
+						int nc = c + dc[k];
+						
+						if(nr<0 || nc<0 || nc >= N || nr >= N) continue;
+						
+						pQ.add(new int[] {nr,nc,1});
+						
+					}
+				}
+			}
 
 			for (int i = 0; i < N; i++) { //r
 				for (int j = 0; j < N; j++) { //c 
-					
-					if(tree[i][j] != null && tree[i][j].size() != 0) {
-						long size = tree[i][j].stream().filter(v-> v%5 == 0).count();
-						
-						if(size > 0) {
-							//가을
-							for (int k = 0; k < 8; k++) {
-								int nr = i + dr[k];
-								int nc = j + dc[k];
-								
-								if(nr<0 || nc< 0 || nr>=N || nc>= N) continue;
-								
-								if(tree[nr][nc] == null ) {
-									tree[nr][nc] = new ArrayList<Integer>();
-								}
-								
-								for (int t = 0; t < size; t++) {
-									tree[nr][nc].add(1); //1살 나무 추가
-								}
-								
-							}
-						}
-						
-					}
-					
 					//겨울
 					map[i][j] += A[i][j];
 				}
@@ -130,16 +121,7 @@ public class BOJ_16235 {
 			year++; //년도 증가
 		}
 		
-		int totalTree = 0;
-		for (int i = 0; i < N; i++) { //r
-			for (int j = 0; j < N; j++) { //c
-				if(tree[i][j] == null ) {
-					continue;
-				}
-				totalTree+=tree[i][j].size();
-			}
-		}
-		System.out.println(totalTree);
+		System.out.println(pQ.size());
 		
 	}
 
