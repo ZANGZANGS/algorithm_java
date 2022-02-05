8208ee5461fce40bebdba39773b29a0e062d2e3e
zangzangs <kimjangh94@gmail.com>
2022-02-05 14:31:48 +0900

    도시 분할 계획 MST

--- algorithm_java/simulation/BOJ_1647.java
+++ algorithm_java/simulation/BOJ_1647.java
@@ -3,6 +3,7 @@ package simulation;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 /**
  * @source		: 백준
@@ -11,11 +12,13 @@ import java.util.StringTokenizer;
  * ==============================================
  * DATE			NOTE	
  * ==============================================
- * 2022.02.04	TODO 실패
- * 				최소 스패닝 트리가 무엇인지 알아야 풀 수 있을 듯한다..
+ * 2022.02.04	실패
+ * 2022.02.05	TODO 답지 보고 겨우 풀었음,, 다시 풀어보자
  */
 public class BOJ_1647 {
-
+	
+	static int[] cycle;
+	
 	public static void main(String[] args) throws IOException{
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -24,14 +27,59 @@ public class BOJ_1647 {
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
 		
-		for (int i = 0; i < M; i++) {
-			st = new StringTokenizer(br.readLine());
-			int A = Integer.parseInt(st.nextToken());
-			int B = Integer.parseInt(st.nextToken());
-			int C = Integer.parseInt(st.nextToken());
+		cycle = new int[N];
+		for (int i = 0; i < N; i++) {
+			cycle[i] = i;
 		}
 		
+		int[][] road = new int[M][3];
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int A = Integer.parseInt(st.nextToken())-1;
+			int B = Integer.parseInt(st.nextToken())-1;
+			int C = Integer.parseInt(st.nextToken());
+			road[i] = new int[]{A,B,C};
+		}
 		
+		Arrays.sort(road,(a,b)->a[2]-b[2]);
+		
+		int count = 0;
+		int total = 0;
+		for (int[] cur : road) {
+			
+			if( count >=N-2) break;
+			int y = cur[0];
+			int x = cur[1];
+			int val = cur[2];
+			
+			if(find(y) != find(x)) {
+				total += val;
+				count++;
+				
+				union(x, y);
+			}
+			
+			
+		}
+
+		System.out.println(total);
+		
+	}
+	
+	private static int find(int tar) {
+		
+		if(tar == cycle[tar]) {
+			return tar;
+		}
+		cycle[tar] = find(cycle[tar]);
+		return cycle[tar];
+	}
+	
+	private static void union(int x, int y) {
+		x = find(x);
+		y = find(y);
+		
+		if(x != y) cycle[y]=x;
 	}
 
 }
